15fefdced5658fd27262fae7f8ab6189b5af66ae
VijethaKaja <69506580+VijethaKaja@users.noreply.github.com>
2020-08-11 14:31:52 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,20 @@
+class Datatypes
+{
+public static void main(String args[])
+{
+byte b = 10;
+short s = 200;
+int num = 13313;
+long l = 146979283L;
+double num1 = 44.3;
+float num2 = 22.4f;
+char ch = 'A';
+String str = "Vijetha";
+System.out.println(b);
+System.out.println(s);
+System.out.println(l);
+System.out.println(num1);
+System.out.println(num2);
+System.out.println(num);
+System.out.println(ch);
+System.out.println(str);
