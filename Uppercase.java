4916dde21736571297264e6c1f565cddedf18824
VijethaKaja <69506580+VijethaKaja@users.noreply.github.com>
2020-08-11 17:28:09 +0530

    Create Uppercase.java

---
+++ Uppercase.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Uppercase {
+public static void main(String[] args)  {
+	String names[]={"Vijetha","Thrylokya","Samyukta"};
+	String temp[]=new String[s1.length];
+	for (int i = 0; i < names.length; i++)
+		temp[i]=names[i].toLowerCase();
+	for (int i = 0; i < temp.length; i++) {
+		System.out.println(temp[i]);
+}
+ 
+}
+}
