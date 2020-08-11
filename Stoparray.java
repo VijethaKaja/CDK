f24bcdf58f149b5980207617a4b2811fd4efde55
VijethaKaja <69506580+VijethaKaja@users.noreply.github.com>
2020-08-11 17:23:42 +0530

    Create Stoparray.java

---
+++ Stoparray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Stoparray {
+    public static void main(String[ ] args) {
+	string s;
+	Scanner sc=new Scanner(System.in);
+	while(true)
+	{
+	s=sc.next();
+	if (s.equals("STOP"))
+		break;
+	System.out.println(s);
+	}
+}
+}
