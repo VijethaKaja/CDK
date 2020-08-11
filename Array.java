e2ca836ea906cb8572d67acd66b5d392628c8d45
VijethaKaja <69506580+VijethaKaja@users.noreply.github.com>
2020-08-11 14:37:24 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public static void main(String[ ] args) {
+    for(String s:args){
+      if(s.length>3)
+       System.out.println("Args "+ s);
+       }
+       
+    }
+}
