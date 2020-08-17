0f1cdfeefa5d1a81408e0aceacdec9035b2eb851
VijethaKaja <69506580+VijethaKaja@users.noreply.github.com>
2020-08-17 10:40:30 +0530

    Create Click button.js

---
+++ Click button.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<script type="text/javascript">
+
+function test1(){
+	
+	console.log(document.getElementById("mynm").value);
+	document.getElementById("btn").value = document.getElementById("mynm").value
+	
+}
+</script>
+
+</head>
+<body>
+Name : <input type="text" id="mynm" /><br/>
+<input id="btn" type="button" value="Click Me" onclick="test1()" />
+<div id="display">
+	aaaaaaaaaaaaaaa
+</div>
+</body>
+</html>
