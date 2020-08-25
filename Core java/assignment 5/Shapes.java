ae2662c775f48012d1fa0f8903dc5536476bb943
VijethaKaja <69506580+VijethaKaja@users.noreply.github.com>
2020-08-25 18:02:34 +0530

    Create Shapes.java

---
+++ Core java/assignment 5/Shapes.java
@@ -0,0 +1,18 @@
+public class Shapes {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Shapes()
+	{
+		
+	}
+	public Shapes(String name)
+	{	super();
+		this.name=name;
+	}
+
+}
