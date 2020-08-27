3c628c2f08c66d7c48f13a50c5637ab99ebc6c40
VijethaKaja <69506580+VijethaKaja@users.noreply.github.com>
2020-08-27 09:48:51 +0530

    Update BookStore.java

--- Core java/assignment6/BookAssignment/pack2/BookStore.java
+++ Core java/assignment6/BookAssignment/pack2/BookStore.java
@@ -1,5 +1,5 @@
-package BookAssignment.pack3;
-import BookAsiignment.pack1.Book;
+package BookAssignment.pack2;
+import BookAssignment.pack1.Book;
 public interface BookStore {
 	public Book[] getBooks();
 	public Book findBookById(long id);
