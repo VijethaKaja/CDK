344afd55be721195cf314a7ab2b58af726d2caf9
VijethaKaja <69506580+VijethaKaja@users.noreply.github.com>
2020-08-28 00:33:50 +0530

    Add files via upload

---
+++ Core java/assignment 8/com/employeeAssignment/EmpIO.java
@@ -0,0 +1,13 @@
+package com.employeeAssignment;
+
+import java.util.List;
+
+import com.pojo.Employee;
+
+public interface EmpIO {
+	 void addToFile(List<Employee>employees);
+	   List<Employee> readFromFile();  
+
+	   Employee readFromUser(); 
+	   void displayToUser(Employee emp); 
+}
